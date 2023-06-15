1e72ebea8b60fde8583d7c64a78d505c00f351bd
Guillermo Segura <guillermo.segura@axity.com>
2023-06-15 10:08:39 -0600

    Se agrega el app de springboot

---
+++ soap-exercise/src/main/java/mx/axity/com/webapi/rest/client/PlayersoapApplication.java
@@ -0,0 +1,13 @@
+package mx.axity.com.webapi.rest.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersoapApplication.class, args);
+	}
+
+}
