b59e6de1878c8cd31ad5223526f399aa7f1395fa
Guillermo Segura <guillermo.segura@axity.com>
2023-06-16 12:07:38 -0600

    Se agregan pruebas unitarias de rest

--- rest-client/src/main/java/mx/axity/com/webapi/rest/client/service/impl/BookClientServiceImpl.java
+++ rest-client/src/main/java/mx/axity/com/webapi/rest/client/service/impl/BookClientServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import com.google.gson.Gson;
