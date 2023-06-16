6937ea147aa0c03e04d7b97e1cf47afc345dde91
Guillermo Segura <guillermo.segura@axity.com>
2023-06-16 15:36:07 -0600

    Se agregan inserts

---
+++ rest-exercise/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO order_table (id, clientId, ts, total) VALUES (1, 1, '2023-06-14 10:30:00', 130.00);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (2, 2, '2023-06-15 15:45:00', 221.50);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (3, 1, '2023-06-16 09:15:00', 30.00);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (4, 3, '2023-06-17 14:20:00', 125.00);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (5, 2, '2023-06-18 11:30:00', 54.00);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (6, 3, '2023-06-19 16:00:00', 40.00);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (7, 1, '2023-06-20 13:45:00', 80.00);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (8, 2, '2023-06-21 10:10:00', 40.00);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (9, 3, '2023-06-22 15:20:00', 40.00);
+INSERT INTO order_table (id, clientId, ts, total) VALUES (10, 1, '2023-06-23 12:00:00', 40.00);
+
+
+
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (1, 1, 'SKU001', 'Producto 1', 2, 50.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (2, 1, 'SKU002', 'Producto 2', 1, 30.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (3, 2, 'SKU003', 'Producto 3', 3, 65.50);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (4, 2, 'SKU004', 'Producto 4', 1, 25.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (5, 3, 'SKU005', 'Producto 5', 2, 15.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (6, 4, 'SKU006', 'Producto 6', 1, 35.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (7, 4, 'SKU007', 'Producto 7', 4, 10.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (8, 4, 'SKU008', 'Producto 8', 2, 25.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (9, 5, 'SKU009', 'Producto 9', 3, 18.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (10, 6, 'SKU010', 'Producto 10', 1, 40.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (11, 7, 'SKU011', 'Producto 11', 1, 40.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (12, 7, 'SKU012', 'Producto 12', 1, 40.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (13, 8, 'SKU013', 'Producto 13', 1, 40.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (14, 9, 'SKU014', 'Producto 14', 1, 40.00);
+INSERT INTO order_detail (id, orderId, sku, description, quantity, price) VALUES (15, 10, 'SKU015', 'Producto 15', 1, 40.00);
